9119c96bc7962b3765293eb6c68183a845d8c945
Igor Follador <igorledf@hotmail.com>
2022-08-17 20:33:53 -0300

    Create initial structure layout

--- frases_do_dia/lib/home.dart
+++ frases_do_dia/lib/home.dart
@@ -8,11 +8,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _frases = [
+    "Como dizia o velho e conhecido ditado: crie uma cobra e eu te direi quem és!",
+    "Nunca abuse de um homem caído, afinal, ele pode se levantar.",
+    "Desistir é para os fracos. Faça como eu, nem tente.",
+    "Nunca cometa um erro duas vezes.Cometa umas 5,só pra ter certeza"
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase";
+
+  void _gerarFrase() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text("Frases do dia"),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("images/frasesDoDia.png"),
+                Text(
+                  _fraseGerada,
+                  textAlign: TextAlign.justify,
+                  style: const TextStyle(
+                      fontSize: 30,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black),
+                ),
+                ElevatedButton(
+                    onPressed: _gerarFrase, child: Text("Nova frase"))
+              ],
+            ),
+          ),
+        ));
   }
 }
